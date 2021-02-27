d3fee65b7997a4c62b8b48b63d550c9d118f9199
mulholio <james@jmulholland.com>
2021-02-27 17:32:21 +0000

    feat: add randomisation

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+import sys
+import random
+
 def greet(name = None):
-    greeting = 'hello'
+    greeting = get_rand_greeting()
     if name:
         print(f"{greeting}, {name}")
         return
     print(greeting)
+
+def get_rand_greeting():
+    return random.choice(["Hi", "Hello", "How you doing"])
 
 if len(sys.argv) > 1:
     name = sys.argv[1]
